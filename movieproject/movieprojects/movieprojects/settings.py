11f8893b14ead39180489e0c2a0237b45f853272
developer <developerdjango660@gmail.com>
2025-06-27 17:18:52 +0530

    added

--- movieproject/movieprojects/movieprojects/settings.py
+++ movieproject/movieprojects/movieprojects/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'movieprojects.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'studyproject',
+        'USER':'root',
+        'PASSWORD':'',
     }
 }
 
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_URL = '/media/'
